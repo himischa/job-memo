270a5013402186ca28d4c5bc572d86b03eef23dd
himischa <mischa.workspace@gmail.com>
2026-07-06 23:41:37 +0800

    feat: add ApplicationDetailPage route and enable navigation from Dashboard

--- job-memo-frontend/src/App.tsx
+++ job-memo-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
+import ApplicationDetailPage from './pages/ApplicationDetailPage';
 
 export default function App() {
   return (
@@ -17,6 +18,14 @@ export default function App() {
             element={
               <ProtectedRoute>
                 <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/applications/:id"
+            element={
+              <ProtectedRoute>
+                <ApplicationDetailPage />
               </ProtectedRoute>
             }
           />
